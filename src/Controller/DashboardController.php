f26dabffaf87152de19310c15e8319b3f1559e0b
judev34 <julien.coudert34@gmail.com>
2025-01-09 16:35:59 +0100

    feat: remove live component and recreate dashboard part1

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -14,6 +14,6 @@ class DashboardController extends AbstractController
     #[Route('', name: 'practitioner_dashboard')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig');
+        return $this->render('dashboard/home.html.twig');
     }
 }
