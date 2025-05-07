f9c0bcdade1199f722cd53c6fc3990b77f007397
judev34 <julien.coudert34@gmail.com>
2025-05-07 13:23:08 +0200

    feat: corrections register login

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -35,7 +35,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('password', RepeatedType::class, [
+            ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les mots de passe doivent correspondre.',
                 'required' => true,
